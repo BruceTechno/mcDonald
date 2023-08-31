9f6b12590f1e3d73a73451843be574b4feafc7e2
Bruce <yu86923@gmail.com>
2023-08-31 11:37:51 +0800

    remove punchin request

--- src/main/java/com/example/mcDonald/vo/request/PunchInRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.mcDonald.vo.request;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-public class PunchInRequest {
-
-}
--- src/main/java/com/example/mcDonald/vo/request/PunchOutRequest.java
+++
@@ -1,4 +0,0 @@
-package com.example.mcDonald.vo.request;
-
-public class PunchOutRequest {
-}
